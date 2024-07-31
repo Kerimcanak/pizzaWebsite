a3460c966e30eb68fe8d6dcd2aea3469b1499a61
Kerim <kerimcanak@gmail.com>
2024-08-01 01:32:56 +0300

    Update HomePageMainBottom.jsx

--- src/components/HomePageMain/HomePageMainBottom.jsx
+++ src/components/HomePageMain/HomePageMainBottom.jsx
@@ -1,21 +1,31 @@
-import { Button, ButtonGroup } from "reactstrap"
+import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Container, Row } from "reactstrap"
 
 function HomePageMainBottom () {
     return (
         <>
-            <div style={{width: '100vw', height: '100vh', backgroundColor: '#faf7f2'}}>
-            <ButtonGroup>
-            <Button>
-                Left
-            </Button>
-            <Button>
-                Middle
-            </Button>
-            <Button>
-                Right
-            </Button>
-            </ButtonGroup>
-            </div>
+                <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                    {Array.from({ length: 6 }).map((_, index) => (
+                    <Button key={index} color="white" style={{ color: 'black', margin: '0 5px' }}>
+                        Button {index + 1}
+                    </Button>
+                    ))}
+                </div>
+                <Row>
+                    {Array.from({ length: 4 }).map((_, index) => (
+                    <Col sm="3" key={index}>
+                        <Card>
+                        <CardImg top width="100%" src={`https://picsum.photos/200/150?random=${index}`} alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle tag="h5">Card Title {index + 1}</CardTitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                        </Card>
+                    </Col>
+                    ))}
+                </Row>
+                </Container>
         </>
     )
 }
