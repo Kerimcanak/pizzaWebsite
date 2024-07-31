48e6490b127beb8994d821148fb807d270136480
Kerim <kerimcanak@gmail.com>
2024-07-31 12:43:21 +0300

    Update HomePageHero.jsx

--- src/components/homePageHero/HomePageHero.jsx
+++ src/components/homePageHero/HomePageHero.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardImg, CardTitle } from "reactstrap";
+import { Button, Card, CardImg, CardTitle, Nav, Navbar, NavItem, NavLink } from "reactstrap";
 import homebanner from "/home-banner.png";
 import logo from "/logo.svg"
 import "./HomePageHero.css"
@@ -37,6 +37,47 @@ function HomePageHero() {
                     </Button>                
                 </div>
             </Card>
+
+            <Navbar id="#1" color="light" light expand="md">
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="YENİ!" />
+                            <span> YENİ! Kore</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="Pizza" />
+                            <span> Pizza</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="Burger" />
+                            <span> Burger</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="Kızartmalar" />
+                            <span> Kızartmalar</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="Fast food" />
+                            <span> Fast food</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <img src="https://picsum.photos/40/40" alt="Gazlı İçecek" />
+                            <span> Gazlı İçecek</span>
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
 
 
         </>
