ba91df682aae93fc2cf084c24a631126a84d60ab
Kerim <kerimcanak@gmail.com>
2024-07-31 13:42:45 +0300

    Update HomePageMain.jsx

--- src/components/HomePageMain/HomePageMain.jsx
+++ src/components/HomePageMain/HomePageMain.jsx
@@ -1,10 +1,65 @@
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from "reactstrap"
+
 function HomePageMain () {
     return (
         <>
-            <div style={{width: "100vw", height: "100vh", backgroundColor: "#faf7f2"}}>
 
-                
-                
+<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start', width: '100vw', height: '100vh', backgroundColor: '#faf7f2'}}>
+ 
+                <Card
+                style={{
+                    width: '18rem',
+                    color: 'white',
+                    backgroundImage: 'url(https://picsum.photos/200/300)',
+                    backgroundSize: 'cover'
+                }}
+                >
+                <CardBody>
+                    <CardTitle tag="h5">
+                    Card title
+                    </CardTitle>
+                    <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                    >
+                    Card subtitle
+                    </CardSubtitle>
+                    <CardText>
+                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                    </CardText>
+                    <Button>
+                    Button
+                    </Button>
+                </CardBody>
+                </Card>
+
+                <Card
+                style={{
+                    width: '18rem',
+                    color: 'white',
+                    backgroundImage: 'url(https://picsum.photos/200/300)',
+                    backgroundSize: 'cover'
+                }}
+                >
+                <CardBody>
+                    <CardTitle tag="h5">
+                    Card title
+                    </CardTitle>
+                    <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                    >
+                    Card subtitle
+                    </CardSubtitle>
+                    <CardText>
+                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                    </CardText>
+                    <Button>
+                    Button
+                    </Button>
+                </CardBody>
+                </Card>
+
             </div>
         </>
     )
