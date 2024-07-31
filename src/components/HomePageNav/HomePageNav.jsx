6feb24364ce79e6841f66bb9fdd2cf69352d015b
Kerim <kerimcanak@gmail.com>
2024-07-31 13:20:20 +0300

    stylising navbar ended

--- src/components/HomePageNav/HomePageNav.jsx
+++ src/components/HomePageNav/HomePageNav.jsx
@@ -4,46 +4,47 @@ function HomePageNav() {
 
     return (
         <>
-         <Navbar id="contents" color="light" light expand="md">
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="YENİ!" />
-                            <span> YENİ! Kore</span>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="Pizza" />
-                            <span> Pizza</span>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="Burger" />
-                            <span> Burger</span>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="Kızartmalar" />
-                            <span> Kızartmalar</span>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="Fast food" />
-                            <span> Fast food</span>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">
-                            <img src="https://picsum.photos/40/40" alt="Gazlı İçecek" />
-                            <span> Gazlı İçecek</span>
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar>
+         <Navbar id="contents" className="bg-light" light expand="md">
+    <Nav className="mx-auto justify-content-center" navbar>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="YENİ!" />
+                <span> YENİ! Kore</span>
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="Pizza" />
+                <span> Pizza</span>
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="Burger" />
+                <span> Burger</span>
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="Kızartmalar" />
+                <span> Kızartmalar</span>
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="Fast food" />
+                <span> Fast food</span>
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#">
+                <img src="https://picsum.photos/40/40" alt="Gazlı İçecek" />
+                <span> Gazlı İçecek</span>
+            </NavLink>
+        </NavItem>
+    </Nav>
+</Navbar>
+
 
             
         </>
