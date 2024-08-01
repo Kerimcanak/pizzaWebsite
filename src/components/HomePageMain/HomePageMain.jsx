d18d1ced11f4958d5cf0a620802e27b553b69a7f
Kerim <kerimcanak@gmail.com>
2024-08-01 11:00:32 +0300

    Update HomePageMain.jsx

--- src/components/HomePageMain/HomePageMain.jsx
+++ src/components/HomePageMain/HomePageMain.jsx
@@ -1,99 +1,43 @@
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from "reactstrap"
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Col, Container, Row } from "reactstrap"
 
 function HomePageMain () {
     return (
         <>
 
-<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start', width: '100vw', height: '90vh', backgroundColor: '#faf7f2', paddingTop: '100px', gap: '20px'}}>
- 
-                <Card
-                style={{
-                    width: '18rem',
-                    color: 'white',
-                    backgroundImage: 'url(https://picsum.photos/200/300)',
-                    backgroundSize: 'cover'
-                }}
-                >
+<Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100vw', height: '90vh', backgroundColor: '#faf7f2', paddingTop: '100px', gap: '20px' }}>
+    <Row>
+        <Col style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Card style={{ width: '18rem', color: 'white', backgroundImage: 'url(https://picsum.photos/200/300)', backgroundSize: 'cover' }}>
                 <CardBody>
-                    <CardTitle tag="h5">
-                    Card title
-                    </CardTitle>
-                    <CardSubtitle
-                    className="mb-2 text-muted"
-                    tag="h6"
-                    >
-                    Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                    </CardText>
-                    <Button>
-                    Button
-                    </Button>
+                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card‘s content.</CardText>
+                    <Button>Button</Button>
                 </CardBody>
-                </Card>
-
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                <Card
-                style={{
-                    width: '18rem',
-                    color: 'white',
-                    backgroundImage: 'url(https://picsum.photos/200/300)',
-                    backgroundSize: 'cover'
-                }}
-                >
+            </Card>
+        </Col>
+        <Col style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Card style={{ width: '18rem', color: 'white', backgroundImage: 'url(https://picsum.photos/200/300)', backgroundSize: 'cover' }}>
                 <CardBody>
-                    <CardTitle tag="h5">
-                    Card title
-                    </CardTitle>
-                    <CardSubtitle
-                    className="mb-2 text-muted"
-                    tag="h6"
-                    >
-                    Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                    </CardText>
-                    <Button>
-                    Button
-                    </Button>
+                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card‘s content.</CardText>
+                    <Button>Button</Button>
                 </CardBody>
-                </Card>
-
-                <Card
-                style={{
-                    width: '18rem',
-                    color: 'white',
-                    backgroundImage: 'url(https://picsum.photos/200/300)',
-                    backgroundSize: 'cover'
-                }}
-                >
+            </Card>
+        </Col>
+        <Col style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Card style={{ width: '18rem', color: 'white', backgroundImage: 'url(https://picsum.photos/200/300)', backgroundSize: 'cover' }}>
                 <CardBody>
-                    <CardTitle tag="h5">
-                    Card title
-                    </CardTitle>
-                    <CardSubtitle
-                    className="mb-2 text-muted"
-                    tag="h6"
-                    >
-                    Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                    </CardText>
-                    <Button>
-                    Button
-                    </Button>
+                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card‘s content.</CardText>
+                    <Button>Button</Button>
                 </CardBody>
-                </Card>
-                </div>
-
-                
-
-            </div>
-
-            
+            </Card>
+        </Col>
+    </Row>
+</Container>
 
         </>
     )
