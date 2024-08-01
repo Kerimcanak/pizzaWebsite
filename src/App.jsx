d7a80f93120c76cb7ff4f1722c5d7666f336970d
Kerim <kerimcanak@gmail.com>
2024-08-01 22:09:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import OrderPizzaITwo from './pages/OrderPizza';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/orderpizza" element={<OrderPizzaITwo />} />
     </Routes>
     </BrowserRouter>
   );
